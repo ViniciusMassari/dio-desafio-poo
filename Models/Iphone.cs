710d7d8d1226e14aad702dfb0e3d14d1d8cb3d39
Vinícius Massari <stonefields11@hotmail.com>
2023-10-18 19:51:31 -0300

    :tada: initial commit

---
+++ Models/Iphone.cs
@@ -0,0 +1,17 @@
+namespace desafio_poo.Models
+{
+    public class Iphone : Smartphone
+    {
+
+        public Iphone(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria)
+        {
+
+        }
+        public override void InstalarAplicativo(string nome)
+        {
+            Console.WriteLine("Instalando aplicativo" + nome);
+
+        }
+
+    }
+}
---
+++ Models/Nokia.cs
@@ -0,0 +1,14 @@
+namespace desafio_poo.Models
+{
+    public class Nokia : Smartphone
+    {
+        public Nokia(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria)
+        {
+        }
+
+        public override void InstalarAplicativo(string nome)
+        {
+            Console.WriteLine("Instalando aplicativo" + nome);
+        }
+    }
+}
